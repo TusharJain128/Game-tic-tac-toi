6caed63bb38d564903673768ceca7a315a0c83c5
TusharJain128 <118379406+TusharJain128@users.noreply.github.com>
2023-05-12 17:39:32 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import Board from './Components/Board'
 
 const rootElement = document.getElementById('root');
@@ -8,10 +7,6 @@ const root = createRoot(rootElement);
 
 root.render(
   <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Board/>}/>
-      </Routes>
-    </BrowserRouter>
+    <Board/>
   </>
 );
