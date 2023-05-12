f991b88b2e2af92e748d99a5b32cbe83b9aab55d
TusharJain128 <118379406+TusharJain128@users.noreply.github.com>
2023-05-12 17:40:44 +0530

    Update Board.js

--- src/Components/Board.js
+++ src/Components/Board.js
@@ -1,13 +1,10 @@
 import React, { useState } from 'react'
 import Square from './Square'
-import { useNavigate } from 'react-router-dom'
 
 const Board = () => {
 
     const [state, setState] = useState(Array(9).fill(null))
     const [isXTurn, setIsXTurn] = useState(true)
-    const [winnerName, setWinnerName] = useState("")
-    const navigate = useNavigate()
 
     const logic = () => {
         const square = [
@@ -21,64 +18,61 @@ const Board = () => {
             [2, 4, 6]
         ]
 
-        // let winner = null
         for (let box of square) {
             const [a, b, c] = [...box]
             if (state[a] != null && state[a] === state[b] && state[b] === state[c]) {
-                return true
-                // winner = a
+                return state[a]
             }
         }
-    }
-    const isWinner = logic()
-    if (!isWinner) {
-        var handleClick = (index) => {
-            const copyState = [...state];
-            copyState[index] = isXTurn ? "X" : "O";
-            setWinnerName(copyState[index])
-            // setState(copyState[index]); //? not working , bcoz now state(array) have only copyState(index)=x value that means state has x
-            setState(copyState)
-            setIsXTurn(!isXTurn)
-        }
-    }
-    else{
-        var handleClick = (index) => {
-            // e.preventDefault()
-            navigate('/')
-        }
+        return false
     }
 
+    const isWinner = logic()
+
+    var handleClick = (index) => { // Escape OverLapping
+        if (state[index] != null) {
+            return;
+        }
+
+        const copyState = [...state];
+        copyState[index] = isXTurn ? "X" : "O";
+        setState(copyState)
+        setIsXTurn(!isXTurn)
+
+    }
+    const reset = () => { //resets all square
+        setState(Array(9).fill(null))
+    }
+
+
     return (
-        <div align="center">
+        <>
             <div className="container m-5">
-                <div className="container d-flex  align-items-center">
-                    <Square SquareValue={() => handleClick(0)} value={state[0]} />
-                    <Square SquareValue={() => handleClick(1)} value={state[1]} />
-                    <Square SquareValue={() => handleClick(2)} value={state[2]} />
-                </div>
-                <div className="container d-flex  align-items-center">
-                    <Square value={state[3]} SquareValue={() => handleClick(3)} />
-                    <Square value={state[4]} SquareValue={() => handleClick(4)} />
-                    <Square value={state[5]} SquareValue={() => handleClick(5)} />
-                </div>
-                <div className="container d-flex  align-items-center">
-                    <Square value={state[6]} SquareValue={() => handleClick(6)} />
-                    <Square value={state[7]} SquareValue={() => handleClick(7)} />
-                    <Square value={state[8]} SquareValue={() => handleClick(8)} />
-                </div>
-            </div>
-            <div className="container">
                 {
-                    isWinner && (
-                        <>
-                            <h1>{`winner ${winnerName} player`}</h1>
-                            <h3>Do you want to play again</h3>
-                            <button onClick={() => window.location.reload()}>Yes</button>
-                        </>
-                    )
-                }
+                    isWinner ? (
+                        <div className='container'>
+                            <h1 className='text-primary'>{`Wow, ${isWinner} Won`}</h1>
+                            <button className="btn btn-outline-success" onClick={reset}> Play Again</button>
+                        </div>) : (<>
+                            <h4 className='text-info m-3'>{`${isXTurn ? "X" : "O"} Move`}</h4>
+                            <div className="container d-flex  align-items-center">
+                                <Square SquareValue={() => handleClick(0)} value={state[0]} />
+                                <Square SquareValue={() => handleClick(1)} value={state[1]} />
+                                <Square SquareValue={() => handleClick(2)} value={state[2]} />
+                            </div>
+                            <div className="container d-flex  align-items-center">
+                                <Square value={state[3]} SquareValue={() => handleClick(3)} />
+                                <Square value={state[4]} SquareValue={() => handleClick(4)} />
+                                <Square value={state[5]} SquareValue={() => handleClick(5)} />
+                            </div>
+                            <div className="container d-flex  align-items-center">
+                                <Square value={state[6]} SquareValue={() => handleClick(6)} />
+                                <Square value={state[7]} SquareValue={() => handleClick(7)} />
+                                <Square value={state[8]} SquareValue={() => handleClick(8)} />
+                            </div>
+                        </>)}
             </div>
-        </div>
+        </>
     )
 }
 
